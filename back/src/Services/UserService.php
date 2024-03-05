906a5bf083ccb4e753f0b9b5f4cf5388a8c036e0
root <ghenrique@softexpert.com>
2024-03-05 09:13:54 -0300

    verify credentials

--- back/src/Services/UserService.php
+++ back/src/Services/UserService.php
@@ -16,20 +16,23 @@ Class UserService extends Connection {
     }
 
     public static function createUser(){
-        if(self::$username == '' || self::$username == ''){
-            throw new CustomException("Username or password can't be null", 401);
-        }
         try {
-            $password = password_hash(self::$password, PASSWORD_DEFAULT);
-            $user = parent::$connection->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
-            $user->bindParam(":username", self::$username, \PDO::PARAM_STR);
-            $user->bindParam(":password", $password, \PDO::PARAM_STR);
-            $user->execute();   
-            $user->fetch();
-            return json_encode(array("username"=>self::$username,"password"=>self::$password));
-        } catch (\PDOException $e){
-            throw new CustomException("User already exists.", 401);
+            self::verifyCredentials();
+            try {
+                $password = password_hash(self::$password, PASSWORD_DEFAULT);
+                $user = parent::$connection->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
+                $user->bindParam(":username", self::$username, \PDO::PARAM_STR);
+                $user->bindParam(":password", $password, \PDO::PARAM_STR);
+                $user->execute();   
+                $user->fetch();
+                return json_encode(array("username"=>self::$username,"password"=>self::$password));
+            } catch (\PDOException $e){
+                throw new CustomException("User already exists.", 401);
+            }
+        } catch(CustomException $error){
+            throw new CustomException($error->getMessage(), 401);
         }
+        
     }
 
     public static function connectUser(){
@@ -57,5 +60,31 @@ Class UserService extends Connection {
             return json_encode(array("disconnected"=>true));
         }
         throw new CustomException("User not logged");    
+    }
+
+    public static function verifyCredentials(){
+        if(self::$username == '' || self::$username == ''){
+            throw new CustomException("Username or password can't be null", 401);
+        }
+        if (strlen(self::$username) < 4){
+            throw new CustomException("Username have to be longer then 4 characters.", 401);
+        }if (strlen(self::$password) < 4 || strlen(self::$password) > 16){
+            throw new CustomException("Password have to be longer then 4 characters and lower than 16 characters.", 401);
+        }
+        if (preg_match("/\s/", self::$password)) {
+            throw new CustomException("Password should not contain any white space.", 401);
+        }
+        // if (!preg_match("/\d/", self::$password)) {
+        //     throw new CustomException("Password should contain at least one digit.", 401) ;
+        // }
+        // if (!preg_match("/[A-Z]/", self::$password)) {
+        //     throw new CustomException("Password should contain at least one uppercase character.", 401) ;
+        // }
+        // if (!preg_match("/[a-z]/", self::$password)) {
+        //     throw new CustomException("Password should contain at least one lowercase character.", 401) ;
+        // }
+        // if (!preg_match("/\W/", self::$password)) {
+        //     throw new CustomException("Password should contain at least one special character.", 401);
+        // }
     }
 }
